6e352a34a58484b62d61e5bda9a8458a4f9d3279
YASMEENELSHAMY <147400556+YASMEENELSHAMY@users.noreply.github.com>
2023-12-17 00:35:07 +0200

    Update Player_Four_in_a_row.cpp

--- Player_Four_in_a_row.cpp
+++ Player_Four_in_a_row.cpp
@@ -1,1 +1,21 @@
 
+#include<iostream>
+#include<random>
+#include<iomanip>
+#include<algorithm>
+#include"BoardGame_Classes.hpp"
+using namespace std;
+Player_Four_in_a_row::Player_Four_in_a_row(char symbol) : Player(symbol){}
+Player_Four_in_a_row::Player_Four_in_a_row(int order,char symbol): Player(order,symbol) {}
+void Player_Four_in_a_row::get_move(int& x,int& y) {
+    cout << "\nPlease enter the column number: ";
+    cin >> y;
+}
+string Player_Four_in_a_row::to_string(){
+    return "Player: " + name ;
+}
+
+// Get symbol used by player
+char Player_Four_in_a_row::get_symbol() {
+    return symbol;
+}
